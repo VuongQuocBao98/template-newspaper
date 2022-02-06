f87db1704279fc41dfe1eece427b28ae885fc1b4
VuongQuocBao98 <vuongquocbao98@gmail.com>
2022-02-06 16:57:37 +0700

    first commit

---
+++ owlcarousel/slider.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false, //bật tắt thanh <>
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    })
+});
